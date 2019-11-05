586c38f6524314c0f6f19c66ce1e9e57b7d0c24f
chardbres <richard.m.bresnahan@gmail.com>
2019-11-05 11:07:29 -0500

    Created UI events for message creation and indexing
    Completed functionality to create and index messages. Handlebars script pulls all messages to populate '.messages' <div>.

--- assets/scripts/messages/ui.js
+++ assets/scripts/messages/ui.js
@@ -1,5 +1,38 @@
 'use strict'
 
-const msgApi = require('./api.js')
+// const msgApi = require('./api.js')
 const store = require('../store')
-const msgsPageTemplate = require('../templates/msg-listing.handlebars')
+const msgIndexTemplate = require('../templates/msg-listing.handlebars')
+
+// Message creation success and failure UI
+const onCreateMsgSuccess = responseData => {
+  store.msg = responseData.msg
+  console.log('Message created!')
+}
+
+const onCreateMsgFailure = () => {
+  console.log('Failed to create message!')
+}
+// ----------
+
+// Message index success and failure UI
+const onIndexSuccess = responseData => {
+  store.msgs = responseData.msgs
+  console.log('Got all messages!')
+  console.log(responseData)
+  // This will inovke the handlebars script to populate the user view with all messages
+  const msgIndexHtml = msgIndexTemplate({ msgs: responseData.msgs })
+  $('.messages').html(msgIndexHtml)
+}
+
+const onIndexFailure = () => {
+  console.log('Failed to retrieve messages!')
+}
+// ----------
+
+module.exports = {
+  onCreateMsgSuccess,
+  onCreateMsgFailure,
+  onIndexSuccess,
+  onIndexFailure
+}
