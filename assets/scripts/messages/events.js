45a287494716ce67fe0daeda91bcf2cf6d27445f
chardbres <richard.m.bresnahan@gmail.com>
2019-11-05 15:12:20 -0500

    Complete initial message UI functions and add socket.io scripts to message CRUD events.
    Completed inital fucntions for all message CRUD events in events.js/api.js/ui.js, and attempted first pass at socket.io functionlity for message creation, update, and delete.

--- assets/scripts/messages/events.js
+++ assets/scripts/messages/events.js
@@ -3,15 +3,21 @@
 const getFormFields = require('../../../lib/get-form-fields.js')
 const msgApi = require('./api.js')
 const msgUi = require('./ui.js')
+const io = require('socket.io')
+
+// Makes the socket.io available to all functions
+const socket = io()
 
 // Creates a message
 const onCreateMsg = function (event) {
   event.preventDefault()
-
-  const msg = new FormData(this)
-  msgApi.createMsg(msg)
+  // Creates new message in the database
+  const formData = getFormFields(event.target)
+  msgApi.createMsg(formData)
     .then(msgUi.onCreateMsgSuccess)
     .catch(msgUi.onCreateMsgFailure)
+  // Upon successful DB creation, emits the new message to the socket.io API for broadcast, and logs the socket response
+    .then(socket.emit('new message', formData, (response) => { console.log(response) }))
 }
 // ----------
 
@@ -25,7 +31,46 @@ const onIndex = event => {
 }
 // ----------
 
+// Get single message, passed from app.js after receiving from socket.io
+const onGetMsg = () => {
+  msgApi.getMsg()
+    .then(msgUi.onGetMsgSuccess)
+    .catch(msgUi.onGetMsgFailure)
+}
+// ----------
+
+// Update message
+const onUpdateMsg = function (event) {
+  event.preventDefault()
+
+  const msgId = $(this).attr('id')
+  const formData = getFormFields($('.update-message')[0])
+  msgApi.updateMsg(msgId, formData)
+    .then(msgUi.onUpdateMsgSuccess)
+    .catch(msgUi.onUpdateMsgFailure)
+  // Upon successful DB update, emits message to socket.io API for broadcast, and logs the socket response
+    .then(socket.emit('update message', formData, msgId, (response) => { console.log(response) }))
+}
+// ----------
+
+// Delete message
+const onDeleteMsg = function (event) {
+  event.preventDefault()
+
+  // Gets the messge ID by the html attribute when the div is clicked
+  const msgId = $(this).attr('id')
+  msgApi.deleteMsg(msgId)
+    .then(msgUi.onDeleteMsgSuccess)
+    .catch(msgUi.onDeleteMsgFailure)
+    // Upon successful DB delete, emits message to socket.io API for broadcast, and logs the socket response
+    .then(socket('delete message', msgId, (response) => { console.log(response) }))
+}
+// ----------
+
 module.exports = {
   onCreateMsg,
-  onIndex
+  onIndex,
+  onGetMsg,
+  onDeleteMsg,
+  onUpdateMsg
 }
